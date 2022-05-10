2a6349e5e3996427971cea8a4e409bc3ee9dfc09
Tonya98 <92362802+Tonya98@users.noreply.github.com>
2022-05-10 12:12:26 +0200

    login provvisorio

--- src/app/features/components/login/components/sign-in/sign-in.component.ts
+++ src/app/features/components/login/components/sign-in/sign-in.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-sign-in',
   templateUrl: './sign-in.component.html',
   styleUrls: ['./sign-in.component.scss']
 })
-export class SignInComponent implements OnInit {
+export class SignInComponent  {
 
-  constructor() { }
+  hide = true;
+  email = new FormControl('', [Validators.required, Validators.email]);
 
-  ngOnInit(): void {
+  getErrorMessage() {
+    if (this.email.hasError('required')) {
+      return 'You must enter a value';
+    }
+
+    return this.email.hasError('email') ? 'Not a valid email' : '';
   }
 
 }
--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from "@angular/core";
 import { FlexLayoutModule } from "@angular/flex-layout";
-
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatButtonModule } from '@angular/material/button';
@@ -8,18 +8,21 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatListModule } from '@angular/material/list';
 import { MatTableModule } from '@angular/material/table';
+import { MatInputModule } from '@angular/material/input';
+
 
 
 const imports = [
     FlexLayoutModule,
-
+    MatFormFieldModule ,
     MatToolbarModule,
     MatSidenavModule,
     MatButtonModule,
     MatIconModule,
     MatDividerModule,
     MatListModule,
-    MatTableModule
+    MatTableModule,
+    MatInputModule
 
 ]
 @NgModule({

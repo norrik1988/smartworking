cd914532058033cb1f305a53b5c8f6436a430bf8
VinCrux <vncgrn@gmail.com>
2022-05-11 17:31:26 +0200

    table

--- src/app/features/components/users/users-table/users-table.component.ts
+++ src/app/features/components/users/users-table/users-table.component.ts
@@ -1,12 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 import { UtentiService } from 'src/app/shared/model/user/service/user.service';
+import { Utente } from 'src/app/shared/model/user/user';
 
+
+const ELEMENT_DATA: Utente[] = []
 @Component({
   selector: 'app-users-table',
   templateUrl: './users-table.component.html',
   styleUrls: ['./users-table.component.scss']
 })
 export class UsersTableComponent implements OnInit {
+  displayedColumns: string[] = ['nome', 'cognome', 'cf'];
+  dataSource = ELEMENT_DATA;
 
   constructor(public utenteService: UtentiService) { }
 

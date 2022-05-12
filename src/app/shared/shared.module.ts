aa89ceccb9189c78ec1b9d4e9ac8e9adf89c7139
raleidon <raleid44@gmail.com>
2022-05-12 16:33:55 +0200

    delete dialog

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,13 +1,11 @@
 import { NgModule } from "@angular/core";
 import { MaterialModule } from "../material/material.module";
 import { CardComponent } from "./components/card/card.component";
-import { DialogComponent } from "./components/dialog/dialog.component";
 import { PaginationComponent } from "./components/pagination/pagination.component";
 import { SearchComponent } from "./components/search/search.component";
 
 const components = [
     CardComponent,
-    DialogComponent,
     PaginationComponent,
     SearchComponent,
 ]

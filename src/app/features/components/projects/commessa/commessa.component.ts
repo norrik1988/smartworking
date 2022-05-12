4470f6c6c1b3dd42cd7ab3bb8d6bb549b4dc8ee4
raleidon <raleid44@gmail.com>
2022-05-12 17:23:12 +0200

    comm fix

--- src/app/features/components/projects/commessa/commessa.component.ts
+++ src/app/features/components/projects/commessa/commessa.component.ts
@@ -1,15 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { AddProjectDialogComponent } from 'src/app/shared/components/dialog/projects/add/add-project-dialog.component';
+import { DeleteProjectDialogComponent } from 'src/app/shared/components/dialog/projects/delete/delete-project-dialog.component';
+import { Commessa } from 'src/app/shared/model/commessa/commessa';
 
 @Component({
   selector: 'app-commessa',
   templateUrl: './commessa.component.html',
   styleUrls: ['./commessa.component.scss']
 })
-export class CommessaComponent implements OnInit {
+export class CommessaComponent {
+  utenteService: any;
 
-  constructor() { }
+  constructor(public dialog: MatDialog) { }
 
-  ngOnInit(): void {
+  openDialog(): void {
+    const dialogRef = this.dialog.open(AddProjectDialogComponent, {
+      width: '250px',
+      //data: { nome: this.nome, cognome: this.cognome, data_di_nascita: this.data_di_Nascita, codice_fiscale: this.codice_fiscale }
+    });
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed');
+    })
   }
-
+  openDelete(utente: Commessa) {
+    this.utenteService.userAttuale = utente;
+    const dialogRef = this.dialog.open(DeleteProjectDialogComponent);
+    dialogRef.afterClosed().subscribe(result => {
+      console.log(`Dialog result: ${result}`);
+    });
+  }
 }
+
+

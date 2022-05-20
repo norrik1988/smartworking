9bebe694deb333dc7e401c62439743916a6dd33e
Tonya98 <92362802+Tonya98@users.noreply.github.com>
2022-05-20 12:30:58 +0200

    calendar test

--- src/app/features/components/calendar/datepicker/datepicker.component.ts
+++ src/app/features/components/calendar/datepicker/datepicker.component.ts
@@ -1,3 +1,4 @@
+import { taggedTemplate } from '@angular/compiler/src/output/output_ast';
 import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { CalendarDialogComponent } from 'src/app/shared/dialog/calendar-dialog/calendar-dialog.component';
@@ -16,17 +17,33 @@ export class DatepickerComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  openDialog(date:Event): void {
-    const target = date.target as HTMLDivElement;
-    if(target.classList.contains('mat-calendar-body-cell-content')){
-    this.selected= date;
-    const dialogRef = this.dialog.open(CalendarDialogComponent, {
-      width: '250px',
-    });
-    dialogRef.afterClosed().subscribe(result => {
-      console.log('The dialog was closed');
-    })
+//   openDialog(date:Event): void {
+//     const target = date.target as HTMLDivElement;
+
+//     if(target.classList.contains('mat-calendar-body-cell-content') && target.textContent == '1' && '31'){
+//     this.selected= date;
+//     const dialogRef = this.dialog.open(CalendarDialogComponent, {
+//       width: '250px',
+//     });
+//     dialogRef.afterClosed().subscribe(result => {
+//       console.log('The dialog was closed');
+//     })
+//   }
+// }
+openDialog(date:Event): void {
+  const target = date.target as HTMLDivElement;
+  const day = Number(target.textContent)
+if (target.classList.contains('mat-calendar-body-cell-content')  && day >= 1 && day <= 31 ){
+  this.selected= date;
+     const dialogRef = this.dialog.open(CalendarDialogComponent, {
+       width: '250px',
+     });
+     dialogRef.afterClosed().subscribe(result => {
+       console.log('The dialog was closed');
+     })
+   }
   }
-}
 
 }
+
+

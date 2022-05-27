a47750ec14332396879bafd53051b11ecf1575d7
raleidon <raleid44@gmail.com>
2022-05-27 11:35:45 +0200

    stato boh

--- src/app/features/components/dashboard/home/home.component.ts
+++ src/app/features/components/dashboard/home/home.component.ts
@@ -3,7 +3,18 @@ import { MatDialog } from '@angular/material/dialog';
 import { EditDashboardDialogComponent } from 'src/app/shared/components/dialog/dashboard/edit-dashboard-dialog/edit-dashboard-dialog.component';
 import { UserService } from 'src/app/shared/model/user/service/user.service';
 
-
+export interface FirstRow {
+  flag: boolean;
+}
+export interface BlankRow {
+  flag: boolean;
+}
+export interface SecondRow {
+  flag: boolean;
+}
+export interface ThirdRow {
+  flag: boolean;
+}
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -11,6 +22,58 @@ import { UserService } from 'src/app/shared/model/user/service/user.service';
 })
 export class HomeComponent {
 
+  cards: FirstRow[] = [{
+    flag: false
+  },
+  {
+    flag: false
+  },
+  {
+    flag: false
+  },
+  {
+    flag: true
+  }, {
+    flag: true
+  },
+  ];
+
+  blank: BlankRow[] = [
+    { flag: false },
+    { flag: false },
+    { flag: false },
+    { flag: false },
+    { flag: false },
+  ]
+
+  cardsTwo: SecondRow[] = [{
+    flag: true
+  },
+  {
+    flag: false
+  },
+  {
+    flag: true,
+  },
+  {
+    flag: false
+  }, {
+    flag: true
+  },];
+  cardsThree: ThirdRow[] = [{
+    flag: true
+  },
+  {
+    flag: false
+  },
+  {
+    flag: false,
+  },
+  {
+    flag: false
+  }, {
+    flag: true
+  },];
   constructor(public dialog: MatDialog, public userService: UserService) { }
 
   public get showName() {

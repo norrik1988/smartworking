6f1d29d3a6c289159371331b4c31f738fbd36865
Mirko <mirko.derosa@synclab.net>
2022-07-01 17:38:10 +0200

    put call

--- src/app/shared/model/workspace/service/workspace.service.ts
+++ src/app/shared/model/workspace/service/workspace.service.ts
@@ -10,7 +10,9 @@ import { WorkSpace } from "../workspace";
 })
 export class WorkSpaceService {
 
-    constructor(private http: HttpClient) { }
+    constructor(private http: HttpClient) { 
+        
+    }
 
     workspace!: WorkSpace[];
 
@@ -19,12 +21,19 @@ export class WorkSpaceService {
 
 
     edit_Workstation(form: NgForm, id: number) {
-        this.http.patch<WorkStation>(`http://localhost:3000/workspace/1`, form)
-            .subscribe(res => {
-                const index = this.workspace[0].workstations.findIndex(ws => ws.id === id);
-                this.workspace[0].workstations[index] = res;
-                this.getWorkspace();
-            })
+        for ( let element of this.workspace[0].workstations ){
+            if (element.id == id){
+                this.http.put<WorkStation>(`http://localhost:3000/workspace/1`, form)
+                .subscribe(res => {
+                    const index = this.workspace[0].workstations.findIndex(ws => ws.id === id);
+                    this.workspace[0].workstations[index] = res;
+                    this.getWorkspace();
+                })
+            }
+            console.log(element);
+        }
+      
+        
     }
 
     getWorkspace(): Observable<WorkSpace[]> {

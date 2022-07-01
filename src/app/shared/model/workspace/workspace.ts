66f8e98a8196da25babd8b5d1d95891af27c355e
Mirko <mirko.derosa@synclab.net>
2022-07-01 11:27:05 +0200

    db

--- src/app/shared/model/workspace/workspace.ts
+++ src/app/shared/model/workspace/workspace.ts
@@ -4,4 +4,7 @@ export class WorkSpace {
     id!: number;
     date_workstation!: Date;
     workstations!: WorkStation[];
+    workstationsTwo!: WorkStation[];
+    workstationsThree!: WorkStation[];
+    
 }

57d8442e0abf2850e880eb98e482f86444e45987
Antonioliccardo <liccardoantonio9@gmail.com>
2022-07-05 16:20:48 +0200

    asa

--- src/app/shared/model/workspace/service/workspace.service.ts
+++ src/app/shared/model/workspace/service/workspace.service.ts
@@ -19,35 +19,52 @@ export class WorkSpaceService {
 
     workspace!: WorkSpace[];
     id!: number;
-    workstationArray: any[] = [];
+    workstationArray: any;
     workstationSelected: WorkStation = {} as WorkStation;
     array !: any[];
 
     edit_Workstation(form: NgForm, id: number) {
         id = this.id
-        //  this.workstationArray = this.workspace
-        this.http.patch<WorkStation>(`http://localhost:3000/workspace/1`, form).subscribe(res => {
-            const index = this.workspace[0].workstations.findIndex(ws => ws.id === id);
-            this.workspace[0].workstations[index].user = res.user;
-            this.workstationArray = this.workspace[0].workstations
-            //  this.workstationArray.push(res.user)
-            console.log('workstationArray ' + JSON.stringify(this.workstationArray))
+        this.workstationArray = this.workspace
+        // console.log('workstationArray 1 ' + JSON.stringify(this.workstationArray))
 
-        })
-        for (let element of this.workspace[0].workstations) {
-            if (element.id == id) {
+        //console.log('workstationArray 2 ' + JSON.stringify(this.workstationArray))
+        for (let i = 0; i < this.workspace[0].workstations.length; i++) {
+            if (this.workspace[0].workstations[i].id == id) {
+                this.workspace[0].workstations[i].user = form.value
 
-                // this.http.delete(`http://localhost:3000/workspace/1`).subscribe(() => {
-                //     this.workspace.splice(0, 1);
-                // })
-                // this.http.post<WorkSpace>(`http://localhost:3000/workspace`, this.workstationArray).subscribe(res => {
-                //     this.workspace.push(res)
-                // })
-                // console.log('WORKSPACE ' + JSON.stringify(this.workspace))
-                // console.log('WORKSTATION ARRAY ' + JSON.stringify(this.workstationArray))
             }
+
+            // this.http.post<WorkSpace>(`http://localhost:3000/workspace`, this.workspace[0].workstations[i]).subscribe(res => {
+            //     this.workspace.push(res)
+            // })
+            console.log(JSON.stringify(this.workspace[0].workstations[i]));
+
         }
+
+        //console.log('workstationArray 2 ' + JSON.stringify(this.workstationArray))
+
+        // this.http.patch<WorkStation>(`http://localhost:3000/workspace/1`, form).subscribe(res => {
+        //     const index = this.workspace[0].workstations.findIndex(ws => ws.id === id);
+        //     this.workspace[0].workstations[index].user = res.user;
+        //     this.workstationArray = this.workspace[0].workstations
+        //     this.workstationArray.push(res.user)
+        //     console.log('workstationArray 2 ' + JSON.stringify(this.workstationArray))
+        // })
+        // for (let element of this.workspace[0].workstations) {
+        //     if (element.id == id) {
+        // this.http.delete(`http://localhost:3000/workspace/1`).subscribe(() => {
+        //     this.workspace.splice(0, 1);
+        // })
+        // console.log('workstationArray 2' + JSON.stringify(this.workstationArray))
+        // this.http.post<WorkSpace>(`http://localhost:3000/workspace`, this.workstationArray).subscribe(res => {
+        //     this.workspace.push(res)
+        // })
+        // console.log('WORKSPACE ' + JSON.stringify(this.workspace))
+        // console.log('WORKSTATION ARRAY ' + JSON.stringify(this.workstationArray))
     }
+    //     }
+    // }
     /* this.http.patch<WorkStation>(this.url, form)
                       .subscribe(res => {
                           const index = this.workspace[0].workstations.findIndex(ws => ws.id === id);
